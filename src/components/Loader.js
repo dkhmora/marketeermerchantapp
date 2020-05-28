09da74a00651e6ec33a161a8bd41b12875a9a343
darylmora <daryl_kiel_mora@yahoo.com>
2020-05-28 21:54:14 +0800

    Setup firestore for android and added user-merchant verification

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,9 @@ import React, {setState} from 'react';
 import {StyleSheet} from 'react-native';
 import AnimatedLoader from 'react-native-animated-loader';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
+const merchantsCollection = firestore().collection('merchant_users');
 export default class Loader extends React.Component {
   constructor(props) {
     super(props);
@@ -10,8 +12,29 @@ export default class Loader extends React.Component {
   }
 
   onAuthStateChanged(user) {
-    this.setState({user: user});
-    console.log(user);
+    if (auth().currentUser != null) {
+      merchantsCollection
+        .where(auth().currentUser.uid, '==', true)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.empty) {
+            auth().signOut();
+            console.log('Error: The user does not match with any merchants');
+          } else {
+            snapshot.forEach((doc) => {
+              console.log(
+                `Current user is assigned to Merchant with doc id "${doc.id}"`,
+              );
+            });
+            this.setState({user: user});
+          }
+        })
+        .catch((err) => {
+          console.log(`Error: Cannot read documents - ${err}`);
+        });
+    } else {
+      this.setState({user: user});
+    }
     if (this.state.visible) {
       this.setState({visible: false});
     }
