34bd3a04b0ad7f7c5030d2114b1c690357b43d3f
darylmora <daryl_kiel_mora@yahoo.com>
2020-05-30 02:44:06 +0800

    Added Add Items Screen UI

--- src/navigation/BaseTab.js
+++ src/navigation/BaseTab.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Container} from 'native-base';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import BaseList from '../components/BaseList';
 import {NavigationContainer} from '@react-navigation/native';
+import BaseHeader from '../components/BaseHeader';
 
 const TabBase = createMaterialTopTabNavigator();
 
@@ -22,32 +24,45 @@ const categories = [{
   middleTextKey: 'price'
 }]; */
 
-export default function BaseTab({route}) {
-  const {categories, items, leftTextKey, middleTextKey} = route.params;
+export default function BaseTab({route, navigation}) {
+  const {
+    categories,
+    items,
+    leftTextKey,
+    middleTextKey,
+    fabButton,
+  } = route.params;
 
   const scroll = categories.length > 2 ? true : false;
 
   return (
-    <NavigationContainer theme={NavigationTheme} independent={true}>
-      <TabBase.Navigator tabBarOptions={{scrollEnabled: scroll}}>
-        {categories.map((category, index) => {
-          const categoryItems = items.filter((item) => {
-            return item.category === category;
-          });
-          return (
-            <TabBase.Screen
-              name={`${category}`}
-              component={BaseList}
-              key={index}
-              initialParams={{
-                categoryItems,
-                leftTextKey,
-                middleTextKey,
-              }}
-            />
-          );
-        })}
-      </TabBase.Navigator>
-    </NavigationContainer>
+    <Container style={{flex: 1}}>
+      <BaseHeader title={route.name} optionsButton navigation={navigation} />
+
+      <NavigationContainer theme={NavigationTheme} independent={true}>
+        <TabBase.Navigator tabBarOptions={{scrollEnabled: scroll}}>
+          {categories.map((category, index) => {
+            const pageCategory = category;
+            const categoryItems = items.filter((item) => {
+              return item.category === category;
+            });
+            return (
+              <TabBase.Screen
+                name={`${category}`}
+                component={BaseList}
+                key={index}
+                initialParams={{
+                  pageCategory,
+                  categoryItems,
+                  leftTextKey,
+                  middleTextKey,
+                  fabButton,
+                }}
+              />
+            );
+          })}
+        </TabBase.Navigator>
+      </NavigationContainer>
+    </Container>
   );
 }
