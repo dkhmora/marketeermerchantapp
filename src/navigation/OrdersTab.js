c8f126d1743b7951d1c365c64b7953f1899d6196
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-02 22:07:11 +0800

    Cleaned up files and changed padding in OrdersFlatList

--- src/navigation/OrdersTab.js
+++ src/navigation/OrdersTab.js
@@ -1,109 +1,71 @@
 import React, {Component} from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {Container} from 'native-base';
+// Custom Components
+import OrdersFlatList from '../components/OrdersFlatList';
 import BaseHeader from '../components/BaseHeader';
-import {Container, Text} from 'native-base';
-import {NavigationContainer} from '@react-navigation/native';
-
-import {inject, observer} from 'mobx-react';
-import BaseFlatList from '../components/BaseFlatList';
 
 const TabOrders = createMaterialTopTabNavigator();
 
-@inject('orderStore')
-@inject('authStore')
-@observer
 class OrdersTab extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      orderCategories: [
-        'pending',
-        'accepted',
-        'shipped',
-        'completed',
-        'canceled',
-      ],
-    };
   }
 
-  renderItem = ({item}) => {
-    return <Text>{item.id}</Text>;
-  };
-
   render() {
-    const {orderCategories} = this.state;
     const {name} = this.props.route;
     const {navigation} = this.props;
-
-    const scroll = orderCategories.length > 2 ? true : false;
 
     return (
       <Container>
         <BaseHeader title={name} optionsButton navigation={navigation} />
         <TabOrders.Navigator
           tabBarOptions={{
-            scrollEnabled: scroll,
+            scrollEnabled: true,
             style: {backgroundColor: '#E91E63'},
             activeTintColor: '#fff',
             inactiveTintcolor: '#eee',
             indicatorStyle: {backgroundColor: '#FFEB3B'},
           }}
-          labelStyle={{activeTintColor: '#fff'}}
           headerMode="none">
           <TabOrders.Screen
             name="Pending"
-            component={BaseFlatList}
+            component={OrdersFlatList}
             initialParams={{
               storeFunctionName: 'setPendingOrders',
               storeVarName: 'pendingOrders',
-              leftTextKey: 'orderNumber',
-              middleTextKey: 'user',
-              fabButton: false,
             }}
           />
           <TabOrders.Screen
             name="Accepted"
-            component={BaseFlatList}
+            component={OrdersFlatList}
             initialParams={{
               storeFunctionName: 'setAcceptedOrders',
               storeVarName: 'acceptedOrders',
-              leftTextKey: 'orderNumber',
-              middleTextKey: 'userId',
-              fabButton: false,
             }}
           />
           <TabOrders.Screen
             name="Shipped"
-            component={BaseFlatList}
+            component={OrdersFlatList}
             initialParams={{
               storeFunctionName: 'setShippedOrders',
               storeVarName: 'shippedOrders',
-              leftTextKey: 'orderNumber',
-              middleTextKey: 'userId',
-              fabButton: false,
             }}
           />
           <TabOrders.Screen
             name="Completed"
-            component={BaseFlatList}
+            component={OrdersFlatList}
             initialParams={{
               storeFunctionName: 'setCompletedOrders',
               storeVarName: 'completedOrders',
-              leftTextKey: 'orderNumber',
-              middleTextKey: 'userId',
-              fabButton: false,
             }}
           />
           <TabOrders.Screen
             name="Cancelled"
-            component={BaseFlatList}
+            component={OrdersFlatList}
             initialParams={{
               storeFunctionName: 'setCancelledOrders',
               storeVarName: 'cancelledOrders',
-              leftTextKey: 'orderNumber',
-              middleTextKey: 'userId',
-              fabButton: false,
             }}
           />
         </TabOrders.Navigator>
