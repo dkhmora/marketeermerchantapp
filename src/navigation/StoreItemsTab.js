48560d5379c8c06f53e93cdbe91ada845f9986a9
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-03 18:54:15 +0800

    Deleted unnecessary files

--- src/navigation/StoreItemsTab.js
+++ src/navigation/StoreItemsTab.js
@@ -1,26 +1,11 @@
 import React, {Component} from 'react';
 import {Container} from 'native-base';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import BaseList from '../components/BaseList';
 import ItemsList from '../components/ItemsList';
-import {NavigationContainer, useLinkProps} from '@react-navigation/native';
 import BaseHeader from '../components/BaseHeader';
-import AnimatedLoader from 'react-native-animated-loader';
-import {StyleSheet} from 'react-native';
 import {observer, inject} from 'mobx-react';
 
 const TabBase = createMaterialTopTabNavigator();
-
-const NavigationTheme = {
-  dark: false,
-  colors: {
-    primary: '#5B0EB5',
-    background: '#fff',
-    card: '#fff',
-    text: '#1A191B',
-    border: '#eee',
-  },
-};
 @inject('authStore')
 @inject('detailsStore')
 @inject('itemsStore')
