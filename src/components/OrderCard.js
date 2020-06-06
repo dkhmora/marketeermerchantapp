cc5b3f6a7ef839d26fc35368ff1f38244e5d9540
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-06 16:38:39 +0800

    Orders can now be cancelled, added card in Orders Card header for Order Transaction Fee

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -8,6 +8,8 @@ import {
   Button,
   Right,
   Icon,
+  Toast,
+  View,
 } from 'native-base';
 import {ActionSheetIOS, Platform} from 'react-native';
 import moment, {ISO_8601} from 'moment';
@@ -22,8 +24,16 @@ class OrderCard extends Component {
   }
 
   handleOrderStatus() {
-    const {merchantId, orderId} = this.props;
-    this.props.ordersStore.setOrderStatus(merchantId, orderId);
+    const {merchantId, orderId, orderNumber} = this.props;
+    this.props.ordersStore.setOrderStatus(merchantId, orderId).then(() => {
+      Toast.show({
+        text: `Successfully changed Order # ${orderNumber} status!`,
+        buttonText: 'Okay',
+        type: 'success',
+        duration: 3500,
+        style: {margin: 20, borderRadius: 16},
+      });
+    });
   }
 
   handleViewOrderItems() {
@@ -54,6 +64,19 @@ class OrderCard extends Component {
     });
   }
 
+  handleCancelOrder() {
+    const {merchantId, orderId, orderNumber} = this.props;
+    this.props.ordersStore.cancelOrder(merchantId, orderId).then(() => {
+      Toast.show({
+        text: `Order # ${orderNumber} successfully cancelled!`,
+        buttonText: 'Okay',
+        type: 'success',
+        duration: 3500,
+        style: {margin: 20, borderRadius: 16},
+      });
+    });
+  }
+
   openOptions() {
     ActionSheetIOS.showActionSheetWithOptions(
       {
@@ -65,7 +88,7 @@ class OrderCard extends Component {
         if (buttonIndex === 0) {
           // cancel action
         } else {
-          this.handleDelete();
+          this.handleCancelOrder.bind(this);
         }
       },
     );
@@ -88,17 +111,19 @@ class OrderCard extends Component {
     const buttonText =
       (tabName === 'Accepted' && 'Ship') || (tabName === 'Pending' && 'Accept');
 
-    const footerStatus =
-      tabName === 'Shipped'
-        ? 'Waiting for Customer to Confirm Receipt of Products'
-        : `Order ${tabName}`;
+    const transactionFeeStatus =
+      tabName === 'Pending'
+        ? 'To be charged when Accepted'
+        : 'Charged to account';
 
-    const timeStamp = moment(createdAt, ISO_8601).fromNow();
+    const CardHeader = () => {
+      let optionsButton = false;
 
-    return (
-      <Card {...otherProps} style={{borderRadius: 16, overflow: 'hidden'}}>
+      optionsButton = tabName === 'Pending' && true;
+
+      return (
         <CardItem header bordered style={{backgroundColor: '#E91E63'}}>
-          <Left>
+          <Left style={{flex: 3}}>
             <Body>
               <Text style={{color: '#fff'}}>{userName}</Text>
               <Text note style={{color: '#ddd'}}>
@@ -106,63 +131,77 @@ class OrderCard extends Component {
               </Text>
             </Body>
           </Left>
-          <Right
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              marginLeft: `-30%`,
-            }}>
-            <Text note style={{color: '#ddd'}}>
-              Merchant Fee: ₱{(totalAmount * 0.05).toPrecision(5)}
-            </Text>
-            {Platform.OS === 'ios' ? (
-              <Button
-                transparent
-                onPress={() => this.openOptions()}
-                style={{paddingLeft: 5}}>
-                <Icon name="more" style={{color: '#fff'}} />
-              </Button>
-            ) : (
-              <OptionsMenu
-                customButton={
-                  <Icon active name="more" style={{color: '#fff'}} />
-                }
-                destructiveIndex={1}
-                options={['Cancel Order']}
-                actions={[]}
-              />
-            )}
-          </Right>
-        </CardItem>
-        <CardItem bordered>
-          <Left>
-            <Text>Address:</Text>
-          </Left>
-          <Right>
-            <Text>{userAddress}</Text>
-          </Right>
-        </CardItem>
-        <CardItem bordered>
-          <Left>
-            <Text>Total Amount:</Text>
-          </Left>
-          <Right>
-            <Text>₱{totalAmount}</Text>
-            <Text note>{numberOfItems} items</Text>
-          </Right>
-        </CardItem>
-        <CardItem>
-          <Body>
-            <Button
-              full
-              bordered
-              rounded
-              onPress={this.handleViewOrderItems.bind(this)}>
-              <Text>View Full Order</Text>
-            </Button>
+          <Body style={{flex: 5}}>
+            <Card
+              style={{
+                backgroundColor: '#fff',
+                borderRadius: 16,
+              }}>
+              <CardItem
+                style={{
+                  backgroundColor: 'transparent',
+                }}>
+                <View
+                  style={{
+                    justifyContent: 'flex-start',
+                    alignItems: 'flex-start',
+                  }}>
+                  <Body>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                      }}>
+                      <Text style={{color: '#212121'}}>Transaction Fee: </Text>
+                      <Text style={{color: '#E91E63', fontWeight: 'bold'}}>
+                        ₱{(totalAmount * 0.05).toPrecision(5)}
+                      </Text>
+                    </View>
+                    <Text note style={{color: 'grey', textAlign: 'center'}}>
+                      {transactionFeeStatus}
+                    </Text>
+                  </Body>
+                </View>
+              </CardItem>
+            </Card>
           </Body>
+          {optionsButton && (
+            <Right style={{flex: 1}}>
+              {Platform.OS === 'ios' ? (
+                <Button
+                  transparent
+                  onPress={() => this.openOptions()}
+                  style={{paddingLeft: 5}}>
+                  <Icon name="more" style={{color: '#fff', fontSize: 27}} />
+                </Button>
+              ) : (
+                <OptionsMenu
+                  customButton={
+                    <Icon
+                      active
+                      name="more"
+                      style={{color: '#fff', fontSize: 27}}
+                    />
+                  }
+                  destructiveIndex={1}
+                  options={['Cancel Order']}
+                  actions={[this.handleCancelOrder.bind(this)]}
+                />
+              )}
+            </Right>
+          )}
         </CardItem>
+      );
+    };
+
+    const CardFooter = () => {
+      const footerStatus =
+        tabName === 'Shipped'
+          ? 'Waiting for Customer to Confirm Receipt of Products'
+          : `Order ${tabName}`;
+
+      const timeStamp = moment(createdAt, ISO_8601).fromNow();
+
+      return (
         <CardItem footer bordered>
           <Left>
             <Text note>{timeStamp}</Text>
@@ -180,6 +219,45 @@ class OrderCard extends Component {
             )}
           </Right>
         </CardItem>
+      );
+    };
+
+    return (
+      <Card {...otherProps} style={{borderRadius: 16, overflow: 'hidden'}}>
+        <CardHeader />
+        <CardItem bordered>
+          <Left>
+            <Text>Address:</Text>
+          </Left>
+          <Right>
+            <Text style={{color: '#E91E63', fontWeight: 'bold'}}>
+              {userAddress}
+            </Text>
+          </Right>
+        </CardItem>
+        <CardItem bordered>
+          <Left>
+            <Text>Total Amount:</Text>
+          </Left>
+          <Right>
+            <Text style={{color: '#E91E63', fontWeight: 'bold'}}>
+              ₱{totalAmount}
+            </Text>
+            <Text note>{numberOfItems} items</Text>
+          </Right>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Button
+              full
+              bordered
+              rounded
+              onPress={this.handleViewOrderItems.bind(this)}>
+              <Text>View Full Order</Text>
+            </Button>
+          </Body>
+        </CardItem>
+        <CardFooter />
       </Card>
     );
   }
