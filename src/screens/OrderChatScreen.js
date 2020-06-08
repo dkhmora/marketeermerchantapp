e300c932c3f69c53224282cb16e703745040bc10
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-08 19:17:30 +0800

    Implemented chat functionality for Orders

--- src/screens/OrderChatScreen.js
+++ src/screens/OrderChatScreen.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
+import {View} from 'react-native';
 import {Container, Text} from 'native-base';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import BaseHeader from '../components/BaseHeader';
+import {GiftedChat, Bubble} from 'react-native-gifted-chat';
+import {inject, observer} from 'mobx-react';
 
+@inject('ordersStore')
+@observer
 class OrderChatScreen extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    const {orderId} = this.props.route.params;
+    this.props.ordersStore.getMessages(orderId);
+  }
+
+  onSend(messages = []) {
+    const {orderId} = this.props.route.params;
+    this.props.ordersStore.sendMessage(orderId, messages[0]);
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#E91E63',
+          },
+        }}
+      />
+    );
+  }
+
   render() {
     const {navigation} = this.props;
-    const {name} = this.props.route;
     const {
       userName,
       userAddress,
@@ -20,13 +47,26 @@ class OrderChatScreen extends Component {
 
     const headerTitle = `Order # ${orderNumber} | ${userName}`;
 
+    const {orderMessages} = this.props.ordersStore;
+
     return (
       <Container style={{flex: 1}}>
         <BaseHeader title={headerTitle} backButton navigation={navigation} />
 
-        <SafeAreaView style={{flex: 1}}>
-          <Text>OrderChatScreen</Text>
-        </SafeAreaView>
+        <View style={{flex: 1}}>
+          <GiftedChat
+            textStyle={{color: '#E91E63'}}
+            renderBubble={this.renderBubble}
+            messages={orderMessages}
+            onSend={(messages) => this.onSend(messages)}
+            user={{
+              _id: 1,
+              name: 'user',
+              avatar:
+                'https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/99/UP1675-CUSA11816_00-AV00000000000012//image?_version=00_09_000&platform=chihiro&w=720&h=720&bg_color=000000&opacity=100',
+            }}
+          />
+        </View>
       </Container>
     );
   }
