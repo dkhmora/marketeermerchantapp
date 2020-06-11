3d293b92ee7697e33a8b1a26db55d605beed75bb
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-11 16:50:06 +0800

    Configured notification subscribtions for IOS

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,6 +2,7 @@ import {observable, action} from 'mobx';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import messaging from '@react-native-firebase/messaging';
+import {Platform} from 'react-native';
 
 const fcmCollection = firestore().collection('merchant_fcm');
 class AuthStore {
@@ -24,7 +25,7 @@ class AuthStore {
   }
 
   @action async checkNotificationSubscriptionStatus() {
-    messaging()
+    await messaging()
       .getToken()
       .then((token) =>
         fcmCollection
@@ -41,18 +42,34 @@ class AuthStore {
   }
 
   @action async subscribeToNotifications() {
-    messaging()
-      .getToken()
-      .then((token) =>
-        fcmCollection
-          .doc(this.merchantId)
-          .update('fcmTokens', firestore.FieldValue.arrayUnion(token)),
-      )
-      .catch((err) => console.log(err));
+    if (Platform.OS === 'ios') {
+      const authorizationStatus = await messaging().requestPermission();
+
+      if (authorizationStatus) {
+        console.log('Permission status:', authorizationStatus);
+        await messaging()
+          .getToken()
+          .then((token) =>
+            fcmCollection
+              .doc(this.merchantId)
+              .update('fcmTokens', firestore.FieldValue.arrayUnion(token)),
+          )
+          .catch((err) => console.log(err));
+      }
+    } else {
+      await messaging()
+        .getToken()
+        .then((token) =>
+          fcmCollection
+            .doc(this.merchantId)
+            .update('fcmTokens', firestore.FieldValue.arrayUnion(token)),
+        )
+        .catch((err) => console.log(err));
+    }
   }
 
   @action async unsubscribeToNotifications() {
-    messaging()
+    await messaging()
       .getToken()
       .then((token) =>
         fcmCollection
