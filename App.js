5aac167fc6969779b2edb4ecd0786498d41accd5
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-30 03:48:45 +0800

    Fixed getting order messages when empty

--- App.js
+++ App.js
@@ -7,6 +7,7 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
+import {YellowBox} from 'react-native';
 import _ from 'lodash';
 import moment from 'moment';
 import SplashScreen from 'react-native-splash-screen';
@@ -25,6 +26,13 @@ const ordersStore = (window.store = new OrdersStore());
 const authStore = (window.store = new AuthStore());
 const detailsStore = (window.store = new DetailsStore());
 const itemsStore = (window.store = new ItemsStore());
+
+YellowBox.ignoreWarnings([
+  'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
+  'Animated.event now requires a second argument for options',
+  'Require cycle: node_modules',
+  "Warning: Can't perform a React state update on an unmounted component.",
+]);
 
 export default class App extends React.Component {
   componentDidMount() {
