9978b46dc2d6d350c04115614487e7922b5b4fde
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-30 19:07:27 +0800

    Added fonts, Changed up Base Header UI

---
+++ assets/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#E91E63',
+  dark: '#C2185B',
+  light: '#F8BBD0',
+  accent: '#448AFF',
+  text_primary: '#212121',
+  text_secondary: '#757575',
+  icons: '#FFFFFF',
+  divider: '#BDBDBD',
+  statusBar: 'rgba(0,0,0,0.1)',
+};
